f801b45e426cd86863ed38a880fa7436f5e282d0
guidoarnone <guido.arnone@mercadolibre.com>
2015-08-01 21:05:35 -0300

    Function rearrangement to improve readability

--- parser/parser.go
+++ parser/parser.go
@@ -23,6 +23,39 @@ var InvalidElementSyntaxError = errors.New("One of your numbers has an invalid f
 
 type RationalMatrix [][]big.Rat
 
+/*
+ * Parses a rational matrix into a string, for user interaction.
+ */
+
+func (matrix *RationalMatrix) String() string {
+	strMatrix := ""
+	for i := 0; i < len(*matrix); i++ {
+		columnLength := len((*matrix)[i])
+		for j:= 0; j < columnLength; j++ {
+			value := (*matrix)[i][j]
+			//if denominator is one, it's removed from the string
+			if big.NewInt(1).Cmp(value.Denom()) == 0 {
+				strMatrix += value.Num().String()
+			} else {
+				strMatrix += value.String()
+			}
+			if j < columnLength -1 {
+				strMatrix += " "
+			}
+		}
+		strMatrix += "\n"
+	}
+	return strMatrix
+}
+
+/*
+ * Parses a rational matrix into a byte array, for user interaction.
+ */
+
+func (matrix *RationalMatrix) Bytes() []byte {
+	return []byte(matrix.String())
+}
+
 type MatrixElement struct {
 	rowNumber, columnNumber int
 	value big.Rat
@@ -86,39 +119,6 @@ func ParseToRationalMatrix(stringMatrix [][]string) (matrix RationalMatrix, err 
 		err = RationalMatrixParseError
 	}
 	return matrix, err
-}
-
-/*
- * Parses a rational matrix into a string, for user interaction.
- */
-
-func (matrix *RationalMatrix) String() string {
-	strMatrix := ""
-	for i := 0; i < len(*matrix); i++ {
-		columnLength := len((*matrix)[i])
-		for j:= 0; j < columnLength; j++ {
-			value := (*matrix)[i][j]
-			//if denominator is one, it's removed from the string
-			if big.NewInt(1).Cmp(value.Denom()) == 0 {
-				strMatrix += value.Num().String()
-			} else {
-				strMatrix += value.String()
-			}
-			if j < columnLength -1 {
-				strMatrix += " "
-			}
-		}
-		strMatrix += "\n"
-	}
-	return strMatrix
-}
-
-/*
- * Parses a rational matrix into a byte array, for user interaction.
- */
-
-func (matrix *RationalMatrix) Bytes() []byte {
-	return []byte(matrix.String())
 }
 
 /*
